60751319534595a4c4a4f4880894455a1cb87386
Kauanny Sibert <127500258+KauannySibert@users.noreply.github.com>
2023-11-14 08:48:11 -0300

    Add files via upload

--- sigma./script.js
+++ sigma./script.js
@@ -1,9 +1,16 @@
-function fatorial_user(){
-    let n = parseInt(var1.value);
-    let fat = n;
-    while (n > 1)
-    fat = fat * (n- 1)
-    n = n - 1
-}
+function fatorial_user() {
+while(n>=1){
+        pote = Math.pow (2,n);
+        i=n
+        while(i > n) {
+            fat = fat * (i - 1)
+            i = i - 1;
+        }
+        soma = soma + ( pote/fat );
+        n = n - 1;
+    }
 
+    soma = soma * (1/3);
+    alert(soma);    
+}
 
